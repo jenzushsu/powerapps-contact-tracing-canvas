dcab8b7133160e2394e90473bdbe2e41d2d034cc
Jenzus Hsu <jenzushsu@gmail.com>
2020-03-05 17:10:21 +0800

    Update ContactTracingTeamSiteSPOSiteDesignScript.ps1

--- ps/ContactTracingTeamSiteSPOSiteDesignScript.ps1
+++ ps/ContactTracingTeamSiteSPOSiteDesignScript.ps1
@@ -9,8 +9,8 @@ $userCredential = Get-Credential -UserName $adminUPN -Message "Type the password
 Connect-SPOService -Url https://$orgName-admin.sharepoint.com -Credential $userCredential
 
 #Upload SPOSiteScript for the 2 List 
-$internalListSiteScript = Get-Content '<<Path>>\check-in-list.json' -Raw | Add-SPOSiteScript -Title "Internal Check-in List" -Description "Check-in list for internal users" | select Id
-$externalListSiteScript = Get-Content '<<Path>>\external-check-in-list.json' -Raw | Add-SPOSiteScript -Title "External Check-in List" -Description "Check-in list for external users" | select Id
+$internalList = Get-Content '<<Path>>\check-in-list.json' -Raw | Add-SPOSiteScript -Title "Internal Check-in List" -Description "Check-in list for internal users" | select Id
+$externalList = Get-Content '<<Path>>\external-check-in-list.json' -Raw | Add-SPOSiteScript -Title "External Check-in List" -Description "Check-in list for external users" | select Id
 
 #Variable for the new Site Design
 $webTemplate = "64" #64 = Team Site, 68 = Communication Site, 1 = Groupless Team Site
